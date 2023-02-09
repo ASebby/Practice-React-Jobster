b3a7ee3a1c16b03c637b70f24c68182afdc1f4f8
asebby <mycloudisonfire@gmail.com>
2023-02-08 19:20:59 -0600

    first commit

---
+++ src/components/FormRow.js
@@ -0,0 +1,19 @@
+
+const FormRow = ({name, type, value, handleChange, labelText}) => {
+  return (
+    <div className='form-row'>
+    <label htmlFor= {name} className='form-label'>
+      {name || labelText}
+    </label>
+    <input
+      type= {type}
+      name= {name}
+      value={value}
+      onChange={handleChange}
+      className='form-input'
+    />
+  </div>
+  );
+};
+
+export default FormRow;
