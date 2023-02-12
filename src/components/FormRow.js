1cb678dcfb1eb8d0a46ed73e6f62a4a5cb0f9c1c
asebby <mycloudisonfire@gmail.com>
2023-02-11 23:57:43 -0600

    add job functionality added

--- src/components/FormRow.js
+++ src/components/FormRow.js
@@ -1,18 +1,18 @@
-
-const FormRow = ({name, type, value, handleChange, labelText}) => {
+const FormRow = ({ name, type, value, handleChange, labelText }) => {
   return (
     <div className='form-row'>
-    <label htmlFor= {name} className='form-label'>
-      {name || labelText}
-    </label>
-    <input
-      type= {type}
-      name= {name}
-      value={value}
-      onChange={handleChange}
-      className='form-input'
-    />
-  </div>
+      <label htmlFor={name} className='form-label'>
+        {labelText || name}
+      </label>
+      <input
+        id={name}
+        type={type}
+        name={name}
+        value={value}
+        onChange={handleChange}
+        className='form-input'
+      />
+    </div>
   );
 };
 
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,8 @@
 import Logo from './Logo';
 import FormRow from './FormRow';
+import FormRowSelect from './FormRowSelect';
 import BigSidebar from './BigSidebar';
 import Navbar from './Navbar';
 import SmallSidebar from './SmallSidebar';
 
-export { Logo, FormRow, BigSidebar, Navbar, SmallSidebar };
+export { Logo, FormRow, FormRowSelect, BigSidebar, Navbar, SmallSidebar };
