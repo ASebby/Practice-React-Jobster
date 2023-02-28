d87bd62e99fd18e34480aced4d924a52760c0e4c
asebby <mycloudisonfire@gmail.com>
2023-02-27 21:21:07 -0600

    added a debounce function to fix a bug

--- src/components/SearchContainer.js
+++ src/components/SearchContainer.js
@@ -1,21 +1,42 @@
+import { useState, useMemo } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { FormRowSelect, FormRow } from '.';
 import { handleChange, clearFilters } from '../features/allJobs/allJobsSlice';
 import Wrapper from '../assets/wrappers/SearchContainer';
 
 const SearchContainer = () => {
-  const { isLoading, search, searchStatus, searchType, sort, sortOptions } =
+  const [localSearch, setLocalSearch] = useState('');
+
+  const { isLoading, searchStatus, searchType, sort, sortOptions } =
     useSelector((store) => store.allJobs);
+
   const { jobTypeOptions, statusOptions } = useSelector((store) => store.job);
+
   const dispatch = useDispatch();
+
   const handleSearch = (e) => {
-    if (isLoading) return;
     dispatch(handleChange({ name: e.target.name, value: e.target.value }));
   };
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    setLocalSearch('');
     dispatch(clearFilters());
   };
+
+  const debounce = () => {
+    let timeoutID;
+    return (e) => {
+      setLocalSearch(e.target.value);
+      clearTimeout(timeoutID);
+      timeoutID = setTimeout(() => {
+        dispatch(handleChange({ name: e.target.name, value: e.target.value }));
+      }, 1000);
+    };
+  };
+
+  const optimizedDebounce = useMemo(() => debounce(), []);
+
   return (
     <Wrapper>
       <form className='form'>
@@ -24,8 +45,8 @@ const SearchContainer = () => {
           <FormRow
             type='text'
             name='search'
-            value={search}
-            handleChange={handleSearch}
+            value={localSearch}
+            handleChange={optimizedDebounce}
           />
           <FormRowSelect
             labelText='status'
